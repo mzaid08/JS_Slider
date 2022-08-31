53df4f1f4a504544a1005625941b72229913fd12
mzaid08 <imzaidmohd@gmailcom>
2022-08-31 18:34:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+import people from "./data.js";
+// console.log(people);
+
+const container = document.querySelector(".slide-container");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+// set slides
+
+container.innerHTML = people
+  .map((person, slideIndex) => {
+    const { img, name, job, text } = person;
+    // more logic
+    let position = "next";
+    if (slideIndex === 0) {
+      position = "active";
+    }
+    if (slideIndex === people.length - 1) {
+      position = "last";
+    }
+    return `<article class="slide ${position}">
+          <img
+            src="${img}"
+            class="img"
+            alt="${name}"
+          />
+          <h4>${name}</h4>
+          <p class="title">${job}</p>
+          <p class="text">
+            ${text}
+          </p>
+          <div class="quote-icon">
+            <div class="fas fa-quote-right"></div>
+          </div>
+        </article>`;
+  })
+  .join("");
+
+const startslider = (type) => {
+  // console.log("helloworld");
+  // console.log(type);
+  const active = document.querySelector(".active");
+  const last = document.querySelector(".last");
+  let next = active.nextElementSibling;
+  // next Code
+  if (!next) {
+    next = container.firstElementChild;
+  }
+  active.classList.remove(["active"]);
+  last.classList.remove(["last"]);
+  next.classList.remove(["next"]);
+  // prev code
+  if (type === "prev") {
+    active.classList.add("next");
+    last.classList.add("active");
+    next = last.previousElementSibling;
+    if (!next) {
+      next = container.lastElementChild;
+    }
+    next.classList.remove(["next"]);
+    next.classList.add("last");
+    return;
+  }
+  active.classList.add(["last"]);
+  last.classList.add(["next"]);
+  next.classList.add(["active"]);
+};
+
+nextBtn.addEventListener("click", () => {
+  startslider();
+});
+
+prevBtn.addEventListener("click", () => {
+  startslider("prev");
+});
